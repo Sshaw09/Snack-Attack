6fbf79fdfc21fce2391261ccf7d88d3d47197527
Sshaw09 <97680673+Sshaw09@users.noreply.github.com>
2023-10-07 20:38:19 -0400

    Updates

---
+++ Assets/Script/BadCollectible.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadCollectible : MonoBehaviour
+{
+    public float timer = 3;
+    private int pointValue = -GameManager.score;
+    //public ParticleSystem explosionParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            GameManager.score += pointValue;
+            //Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+
+        }
+    }
+}
